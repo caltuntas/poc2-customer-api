6f50cbb1e6407f43325b81e57f691aee2726091e
Cihat Altuntaş <cihat.altuntas@ericsson.com>
2020-01-20 15:25:37 +0300

    chore: project setup

---
+++ src/main/java/com/ericsson/poc2customerapi/Poc2CustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.poc2customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc2CustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc2CustomerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericsson/poc2customerapi/Poc2CustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.poc2customerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc2CustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
